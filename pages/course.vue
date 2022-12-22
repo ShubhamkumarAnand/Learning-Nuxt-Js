37ec59f219abe3320990005fe65b292b30d90db9
imskanand <the.anand3099@gmail.com>
2022-12-22 13:36:45 +0530

    Learned File Based Routing in Nuxt Js

---
+++ pages/course.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="flex flex-col items-center w-full h-full min-h-screen p-12 bg-gray-100">
+		<div class="mb-12 prose">
+			<h1>
+				<span class="font-medium"> Course: <span class="font-bold">Mastering Nuxt</span> </span>
+			</h1>
+		</div>
+		<div class="flex flex-row justify-center flex-grow">
+			<div class="flex flex-col p-8 mr-4 prose bg-white rounded-md min-w-[20ch]">
+				<h3>Chapters</h3>
+			</div>
+			<div class="p-12 prose bg-white rounded-md w-[65ch]">
+				<h2>Lesson</h2>
+				<p>This is a Lesson</p>
+			</div>
+		</div>
+	</div>
+</template>
